033d34a0a2966c61def66895c01c00f90a8ccc96
Michal Pawelczyk [t: @m_pawelczyk] <michal.pawelczyk@gmail.com>
2020-08-15 10:03:21 +0200

    refactor bootstrap data

--- src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
@@ -3,6 +3,8 @@ package guru.sfg.brewery.repositories.security;
 import guru.sfg.brewery.domain.security.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
  * on 12.08.2020
@@ -10,4 +12,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * in project scc-brewery
  */
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String customer);
 }
