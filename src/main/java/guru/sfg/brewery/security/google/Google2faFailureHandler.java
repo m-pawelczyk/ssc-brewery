32b0a107d87d962ac0a1dd176f8f469f34d7594d
Michal Pawelczyk [t: @m_pawelczyk] <michal.pawelczyk@gmail.com>
2020-10-17 10:22:56 +0200

    error handling, exclude static resources

---
+++ src/main/java/guru/sfg/brewery/security/google/Google2faFailureHandler.java
@@ -0,0 +1,31 @@
+package guru.sfg.brewery.security.google;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
+ * on 17.10.2020
+ * created Google2faFailureHandler in guru.sfg.brewery.security.google
+ * in project scc-brewery
+ */
+@Slf4j
+public class Google2faFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        log.debug("Forward to 2fa");
+
+        request.getRequestDispatcher("/user/verify2fa")
+                .forward(request, response);
+
+    }
+}
